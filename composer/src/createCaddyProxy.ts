048c938732776309e8309674a96310a872b72b53
Gal Schlezinger <gal@spitfire.co.il>
2021-12-26 15:43:00 +0200

    convert to docker compose (#1)

---
+++ composer/src/createCaddyProxy.ts
@@ -0,0 +1,47 @@
+export function createCaddyProxy() {
+  let index = 0;
+
+  function usingUpstreams(
+    subdomain: string,
+    port: number
+  ): Record<string, string> {
+    return {
+      ...root(),
+      ...subdomainDefinition(subdomain, {
+        reverse_proxy: `{{upstreams ${port}}}`,
+      }),
+    };
+  }
+
+  function root() {
+    return {
+      caddy: "*.{env.ROOT_DOMAIN}",
+    };
+  }
+
+  function subdomainDefinition(
+    subdomain: string,
+    options: Record<string, string>
+  ): Record<string, string> {
+    const current = ++index;
+    const name = subdomain.replace(/[^A-z0-9_]/g, "");
+    const properties = Object.fromEntries(
+      Object.entries(options).map(([key, value]) => {
+        const newKey = `caddy.${current}_handle.${key}`;
+        return [newKey, value];
+      })
+    );
+
+    return {
+      [`caddy.${current}_@${name}`]: `host ${subdomain}.{env.ROOT_DOMAIN}`,
+      [`caddy.${current}_handle`]: `@${name}`,
+      ...properties,
+    };
+  }
+
+  return {
+    usingUpstreams,
+    subdomainDefinition,
+    root,
+  };
+}
