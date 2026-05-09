a114d77bd6aff839b451b0bbe8c3add7a272fa93
Gal Schlezinger <gal@spitfire.co.il>
2026-05-08 22:11:10 -0400

    create the directory

--- composer/src/machine1.ts
+++ composer/src/machine1.ts
@@ -254,6 +254,8 @@ export default function machine1(): ComposeSpecification {
         container_name: "jellyfin",
         network_mode: "host",
         environment: ["PUID=1000", "PGID=1000", "TZ=Asia/Jerusalem"],
+        entrypoint: "bash",
+        command: ["-c", "mkdir /tmp-transcoding && /init"],
         volumes: [
           `${helpers.config}:/config`,
           `${library.tv}:/data/tvshows`,
